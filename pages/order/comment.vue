a81817a8b0716cb13177fb6ae20f1e3f1124045f
liujun <jun.liu_southchina@convertlab.com>
2025-01-07 10:10:22 +0800

    feat 评论功能

---
+++ pages/order/comment.vue
@@ -0,0 +1,22 @@
+<template>
+  <view class="comment-page">
+    <uni-forms ref="form" :model="formData">
+      <uni-forms-item label="总体评分">
+        <uni-rate v-model="formData.rating" />
+      </uni-forms-item>
+      
+      <uni-forms-item label="评价内容">
+        <textarea 
+          v-model="formData.content"
+          placeholder="请输入评价内容"
+        />
+      </uni-forms-item>
+      
+      <uni-file-picker
+        v-model="formData.images"
+        mode="grid"
+        @success="handleUploadSuccess"
+      />
+    </uni-forms>
+  </view>
+</template> 
